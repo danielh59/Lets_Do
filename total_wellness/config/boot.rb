9561a37de6fe3caf1e1501252da9a4fac82842bf
tak59 <dev.hoyte@gmail.com>
2023-02-28 13:18:36 -0500

    fix

--- total_wellness/config/boot.rb
+++ total_wellness/config/boot.rb
@@ -1,3 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+#Only the first line was present in this fily. Why does bundler/setup all of sudden fix problem.
 require "bundler/setup" # Set up gems listed in the Gemfile.
